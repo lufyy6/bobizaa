e8221e034e3e4593ac6e1517b21351b718e8e69d
lufyy6 <168370515+lufyy6@users.noreply.github.com>
2024-04-30 11:47:38 +0300

    Create sgad2❄.js

---
+++ plugins/sgad2❄.js
@@ -0,0 +1,31 @@
+import similarity from 'similarity'
+const threshold = 0.72
+export async function before(m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/استخدم.*انسحب/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
+        return !0
+    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
+    if (!(id in this.tebakbendera))
+        return this.reply(m.chat, '* [回لقد انتهي هذا السؤال اكتب علم لتظهر أسأله جديده回]*', m)
+    if (m.quoted.id == this.tebakbendera[id][0].id) {
+        let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
+        if (isSurrender) {
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
+            return this.reply(m.chat, '* [❄] طلع فاشل و استسلم[😎] :( !*', m)
+        }
+        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
+
+        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            this.reply(m.chat, `*回┃اجـابـة صـحـيـحـة┃❄ ❯*\n\n*回↞┇الـجـائـزة❄↞${this.tebakbendera[id][2]} نقطه*`, m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
+        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
+            m.reply(`*[✦] لقد كنت علي وشك النجاح[❅]*!`)
+        else
+            this.reply(m.chat, `回┃اجـابـة خـاطـئـة》❄《🚫 ❯`, m)
+    }
+    return !0
+}
+export const exp = 0
